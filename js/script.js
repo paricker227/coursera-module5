1843e8ec24ebf1ae1f95e2c9a9371632e74eeac3
paricker227 <59850471+paricker227@users.noreply.github.com>
2020-02-18 15:04:14 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -85,9 +85,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 console.log("in addEventListener");
 
-  var buildAndShowHTML_value = buildAndShowHomeHTML(categories);
+  var buildAndShowHTML_value = buildAndShowHomeHTML();
   console.log("after running buildAndShowHomeHTML");
-  
+
 
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
